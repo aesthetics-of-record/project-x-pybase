15682a4ebf6baf6cb30cf0b70167d486c22ccf70
aesthetics-of-record <111675679+aesthetics-of-record@users.noreply.github.com>
2024-03-05 00:48:04 +0900

    feat: 시작 디비 설정 추가중..

--- init/create_db.py
+++ init/create_db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from db import Base, engine
 
 # 생성할 데이터베이스 이름 목록
-_databases = ["_users"]
+_databases = ["users"]
 
 # 각 데이터베이스에 대해 CREATE DATABASE 실행
 for db_name in _databases:
@@ -12,6 +12,12 @@ for db_name in _databases:
         print(f"Database {db_name} created successfully")
 
 
-class User(Base):
-    __tablename__ = "notes"
-    id: Mapped[str] = mapped_column(primary_key=True)
+class ModelBase(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+
+class User(ModelBase):
+    __tablename__ = 'notes'
+    content: Mapped[str]
+
+Base.metadata.create_all(bind=engine)
